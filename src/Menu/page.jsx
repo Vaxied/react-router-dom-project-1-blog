114f90fb3cf8450ebbc1d43ad85541b14c5d9797
Vaxied <psgmarcelo@gmail.com>
2024-02-26 19:08:43 -0400

    Creating routes with NavLink and applying styles with isActive

--- src/Menu/page.jsx
+++ src/Menu/page.jsx
@@ -1,5 +1,25 @@
+import routes from '../routes'
+import { NavLink } from 'react-router-dom'
+
 function Menu() {
-    return <nav>Menu</nav>
+    return (
+        <nav>
+            <ul>
+                {routes.map((route) => (
+                    <li key={route.index}>
+                        <NavLink
+                            style={({ isActive }) => ({
+                                color: isActive ? 'red' : 'blue',
+                            })}
+                            to={route.to}
+                        >
+                            {route.text}
+                        </NavLink>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
 }
 
 export default Menu
---
+++ src/routes.jsx
@@ -0,0 +1,19 @@
+const routes = []
+
+routes.push({
+    index: 0,
+    to: '/',
+    text: 'HomePage',
+})
+routes.push({
+    index: 1,
+    to: '/blog',
+    text: 'BlogPage',
+})
+routes.push({
+    index: 2,
+    to: '/profile',
+    text: 'ProfilePage',
+})
+
+export default routes
