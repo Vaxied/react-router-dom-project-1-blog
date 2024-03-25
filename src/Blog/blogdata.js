b8b04f15f7cc60227bdeb13e7d24c9ef75addf73
Vaxied <psgmarcelo@gmail.com>
2024-03-25 13:18:13 -0400

    Added g-h pages and renamed main.jsx to index.jsx

--- src/Blog/blogdata.js
+++ src/Blog/blogdata.js
@@ -1,38 +1,26 @@
 const blogdata = [
     {
+        slug: 'que-es-javascript',
+        title: 'Que es Javascript',
+        content: 'Javascript es un lenguaje de programacion?',
+        author: 'Barbara',
+    },
+    {
         slug: 'que-es-react',
-        title: 'Que es react',
+        title: 'Que es ReactJS',
         content: 'React es una libreria... o un framework?',
         author: 'Josh',
     },
     {
-        slug: 'que-es-MAngular',
-        title: 'Que es Mangular',
-        content: 'Angular es un framework?',
-        author: 'Barbara',
-    },
-    {
-        slug: 'queeee-es-Angular',
-        title: 'Queeee es angular',
-        content: 'Angular es un framework?',
-        author: 'Barbara',
-    },
-    {
-        slug: 'queaaaa-esa-Angular',
-        title: 'Queaaa esa angular',
-        content: 'Angular es un framework?',
-        author: 'Barbara',
-    },
-    {
-        slug: 'que-es-Angular',
-        title: 'Que es angular',
-        content: 'Angular es un framework?',
+        slug: 'que-es-angular',
+        title: 'Que es Angular',
+        content: 'Angular es un framework de Javascript',
         author: 'Barbara',
     },
     {
         slug: 'que-es-vue',
-        title: 'Que es vue',
-        content: 'Vue es un framework?',
+        title: 'Que es VueJS',
+        content: 'Vue es un framework de Javscript',
         author: 'Arnold',
     },
 ]
