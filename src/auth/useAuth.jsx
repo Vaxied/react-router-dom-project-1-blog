dbba8ecb1b1ca8a77c325101d0cc40301b5389b1
Vaxied <psgmarcelo@gmail.com>
2024-03-25 13:43:31 -0400

    Updates

--- src/auth/useAuth.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { AuthContext } from './authProvider'
-
-function useAuth() {
-    const auth = React.useContext(AuthContext)
-    return auth
-}
-
-export default useAuth
